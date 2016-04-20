493d0680533e3f86de4b577a627e718aaf67bb7a
Roman Lorent <romanlorent@gmail.com>
2016-04-20 23:26:02 +0100

    envirnonet var added

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ var app = express();
 var path = require("path");
 var google = require('googleapis');
 var customsearch = google.customsearch('v1');
+var fs = require("fs");
+var mongodb = require('mongodb');
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -15,30 +17,75 @@ app.get("/", function (req, res) {
 });
 //custom search API
 
+var uri = process.env.PROD_MONGODB;
 
+app.get('/search/:se', function (req, res) {
 
-app.get('/search/:query', function (req, res) {
-
-    var request = req.params.query;
+    var request = req.params.se;
+    var query = parseInt(req.query.offset);
     var params = {
         cx: '009883279465385279356:lmbggz3twmu',
         q: request,
-        key: "AIzaSyAeHrDEOr0ASI8G7MfZ8O7XeCtnraPp3AY"
+        key: "AIzaSyAeHrDEOr0ASI8G7MfZ8O7XeCtnraPp3AY",
+        num: query,
+        searchType: "image"
     };
 
 
-    var respond = customsearch.cse.list(params, function (err, response) {
+    customsearch.cse.list(params, function (err, response) {
         if (err) {
-            console.log('Encountered error', err);
+            res.send('Encountered error', err);
         } else {
-            console.log('Response', response);
+            
+            console.log(uri);
+//            var d = new Data();
+//            var dataSearch = d.toUTCString();
+            var dataToDatbase = {
+                term: request,
+                when: new Date().toUTCString()
+            };
+
+            //database interaction
+            mongodb.MongoClient.connect(uri, function (err, db) {
+
+                    if (err) throw err;
+
+                    var search = db.collection('search');
+
+                    search.insert(dataToDatbase);
+                });
+
+                var json = [];
+
+                res.writeHead(200, {
+                    'Content-Type': 'text/plain'
+                });
+
+                for (var i = 0; i < query; i++) {
+                    json.push({
+                        url: response.items[i].link,
+                        snippet: response.items[i].snippet,
+                        thumbnail: response.items[i].image.thumbnailLink,
+                        context: response.items[i].image.contextLink
+                    });
+
+                }
+
+            
+            res.end(JSON.stringify(json));
+
+
+            //            res.end(JSON.stringify(response.items));
         }
     });
-
-
-    res.send(respond);
-
 });
+
+//getting search history
+app.get('/search/:se', function (req, res) {});
+
+
+
+
 
 
 
